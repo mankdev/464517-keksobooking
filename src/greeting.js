de7ec94d09aeb126db3d75faebd0adb28ad2771e
Artem Parhomenko <aparhomenko@devexperts.com>
2018-02-08 22:59:07 -0800

    add colors support

--- src/greeting.js
+++ src/greeting.js
@@ -2,7 +2,9 @@ require(`colors`);
 const pkg = require(`../package.json`);
 
 function emptyHandler() {
-  console.log(`Привет пользователь! Эта программа будет запускать сервер «${pkg.name.bold}». Автор: ${pkg.author.underline}.`);
+  console.log(
+      `Привет пользователь! Эта программа будет запускать сервер «${pkg.name.bold}». Автор: ${pkg.author.underline}.`
+  );
 }
 
 module.exports = {
