89413b35ad794a6f3811fd0f7894492c158a89c5
Artem Parhomenko <aparhomenko@devexperts.com>
2018-03-08 14:26:29 +0300

    move static server to express

--- src/server/index.js
+++ src/server/index.js
@@ -1,57 +1,9 @@
-const http = require(`http`);
-const url = require(`url`);
-const fs = require(`fs`);
+const express = require(`express`);
 const path = require(`path`);
-const {promisify} = require(`util`);
 
-const readFile = promisify(fs.readFile);
-const exists = promisify(fs.exists);
+const server = express();
 
-const STATIC_SOURCE = path.resolve(__dirname, `../../static`);
-const DEFAULT_MIME_TYPE = `text/plain`;
-const MIME_TYPE_MAP = {
-  '.ico': `image/x-icon`,
-  '.html': `text/html`,
-  '.js': `application/javascript`,
-  '.json': `application/json`,
-  '.css': `text/css`,
-  '.png': `image/png`,
-  '.jpg': `image/jpeg`,
-  '.gif': `image/gif`,
-};
-
-const getFilePath = (pathPart) => {
-  if (pathPart === `/`) {
-    return path.join(STATIC_SOURCE, `index.html`);
-  } else {
-    return path.join(STATIC_SOURCE, pathPart);
-  }
-};
-
-const server = http.createServer((req, res) => {
-  const filePath = getFilePath(url.parse(req.url).pathname);
-
-  return exists(filePath)
-      .then((isExists) => isExists && readFile(filePath))
-      .then((data) => {
-        if (data) {
-          const ext = path.extname(filePath);
-          res.setHeader(`Content-type`, MIME_TYPE_MAP[ext] || DEFAULT_MIME_TYPE);
-          res.statusCode = 200;
-          res.statusMessage = `OK`;
-          res.end(data);
-        } else {
-          res.writeHead(404, `Not Found`);
-          res.end();
-        }
-      })
-      .catch((err) => {
-        res.writeHead(500, err.message, {
-          'content-type': DEFAULT_MIME_TYPE
-        });
-        res.end(err.message);
-      });
-});
+server.use(express.static(path.resolve(__dirname, `../../static`)));
 
 module.exports = {
   server
