f3a0374a652450a72bd76135b59dbff3252ecc8e
Artem Parhomenko <aparhomenko@devexperts.com>
2018-03-12 09:13:04 +0300

    create more featured fill scenario

--- src/descriptors/fill.js
+++ src/descriptors/fill.js
@@ -1,4 +1,6 @@
+const https = require(`https`);
 const {offersStore} = require(`../server/offers/store`);
+const {imagesStore} = require(`../server/images/store`);
 const {generateEntities} = require(`../utils/entities.utils`);
 
 const DATA_COUNT = 20;
@@ -6,7 +8,19 @@ const DATA_COUNT = 20;
 function fillHandler() {
   const offers = generateEntities(DATA_COUNT);
 
-  Promise.all(offers.map((item) => offersStore.save(item)))
+  Promise.all(offers.map((item) => {
+    const avatarPath = `/api/offers/${item.date}/avatar`;
+    const {author: {avatar}} = item;
+    item.author.avatar = avatarPath;
+
+    return new Promise((resolve) => {
+      https.get(avatar, (response) => {
+        return resolve(response);
+      });
+    }).then((stream) => {
+      return Promise.all([offersStore.save(item), imagesStore.save(avatarPath, `image/png`, stream)]);
+    });
+  }))
       .then(() => console.log(`Успешно добавлено ${DATA_COUNT} записей`))
       .then(() => process.exit(0));
 }
